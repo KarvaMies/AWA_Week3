6a24eed6f1fb9588c0d9a0572ecca17210502eac
KarvaMies <jeremias.moberg@gmail.com>
2023-06-01 16:32:41 +0300

    task 2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
@@ -11,24 +10,18 @@ let users = []
 let found = 0
 
 router.post("/todo", (req, res) => {
-  found = 0
   let name = req.body.name
   let todo = req.body.task
-  users.forEach(user => {
-    if(user.name == name) {
-      found = 1
-      user.todos.push(todo)
-      res.send("Todo added")
-      return
-    }
-  });
-  if(!found) {
-    let user = {name: name, todos: [todo]}
-    users.push(user)
-  }
-  console.log(users)
 
-  res.send("User added")
-})
+  let foundUser = users.find(user => user.name === name);
+  if (foundUser) {
+    foundUser.todos.push(todo);
+    res.send("Todo added");
+  } else {
+    let newUser = { name: name, todos: [todo] };
+    users.push(newUser);
+    res.send("User added");
+  }
+});
 
 module.exports = router;
